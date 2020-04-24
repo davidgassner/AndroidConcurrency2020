bf3fed5f6965e8630e24a510ade1410abcdc9e89
David Gassner <david.gassner@gmail.com>
2020-04-24 14:13:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/androidconcurrency2020/Constants.kt
@@ -0,0 +1,3 @@
+package com.example.androidconcurrency2020
+
+const val LOG_TAG = "CodeRunner"
---
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.androidconcurrency2020
+
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidconcurrency2020.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize view binding for view object references
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize button click handlers
+        with(binding) {
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
+
+    }
+
+    /**
+     * Run some code
+     */
+    private fun runCode() {
+        log("Running code")
+    }
+
+    /**
+     * Clear log display
+     */
+    private fun clearOutput() {
+        binding.logDisplay.text = ""
+        scrollTextToEnd()
+    }
+
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd() {
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+}
