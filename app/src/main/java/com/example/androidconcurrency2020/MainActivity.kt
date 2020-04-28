2956eaa63e210b8ea69df8ae0b71807497dbde44
David Gassner <david.gassner@gmail.com>
2020-04-28 10:28:01 -0700

    03_01e

--- app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
+++ app/src/main/java/com/example/androidconcurrency2020/MainActivity.kt
@@ -6,6 +6,10 @@ import android.util.Log
 import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidconcurrency2020.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,7 +34,10 @@ class MainActivity : AppCompatActivity() {
      * Run some code
      */
     private fun runCode() {
-
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = fetchSomething()
+            log(result)
+        }
     }
 
     /**
@@ -56,6 +63,11 @@ class MainActivity : AppCompatActivity() {
      */
     private fun scrollTextToEnd() {
         Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+    private suspend fun fetchSomething(): String {
+        delay(2000)
+        return "something from the web"
     }
 
 }
